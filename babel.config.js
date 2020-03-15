bd2e20891c79bba5af2a33bedad34cd6e7bb95bd
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-03-15 21:41:11 +0400

    fix(src): update some configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,34 @@
+const plugins = [
+    [
+        'module-resolver',
+        {
+            root: ['./src'],
+            extensions: [
+                '.ios.ts',
+                '.android.ts',
+                '.ts',
+                '.ios.tsx',
+                '.android.tsx',
+                '.tsx',
+                '.jsx',
+                '.js',
+                '.json'
+            ],
+            alias: {
+                api: './src/api',
+                utils: './src/utils',
+                helpers: './src/helpers',
+                components: './src/components',
+                containers: './src/containers',
+                reduxStore: './src/redux-store'
+            }
+        }
+    ]
+]
+
+const presets = ['module:metro-react-native-babel-preset']
+
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset']
+    presets,
+    plugins
 }
